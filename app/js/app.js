52627a7967f59ce56694a711a8903f40e5561d1f
mejjjor <erik.aouizerate@gmail.com>
2016-02-02 14:27:21 +0100

    add (ugly) youtube api

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 import Player from "./Player";
+var $ = require('jquery');
 
 var domReady = function(callback) {
     document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
@@ -7,3 +8,49 @@ var domReady = function(callback) {
 domReady(function() {
     var player = new Player(document.getElementById("audio"));
 });
+
+function tplawesome(e, t) {
+    var res = e;
+    for (var n = 0; n < t.length; n++) {
+        res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) {
+            return t[n][r]
+        })
+    }
+    return res
+}
+
+$(function() {
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+        // prepare the request
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+        });
+        // execute the request
+        request.execute(function(response) {
+            var results = response.result;
+            $("#results").html("");
+            $.each(results.items, function(index, item) {
+                $.get("tpl/item.html", function(data) {
+                    $("#results").append(tplawesome(data, [{
+                        "title": item.snippet.title,
+                        "videoid": item.id.videoId
+                    }]));
+                });
+            });
+            resetVideoHeight();
+        });
+    });
+
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9 / 16);
+}
+
